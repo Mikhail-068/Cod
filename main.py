29550068b04552f7a24722914556bca50f449c2e
Mikhail (laptop) <89531276499nokia@gmail.com>
2022-04-25 10:07:25 +0300

    Create func >> Cod <<

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from config import dic_table
 
-example = r"\320\237\320\276\320\263\320\276\320\264\320\260.py"
+def Cod(example):
+    list_ex = []
+    temp = 0
+    for i in range(len(example)):
+        if (i != 0) and (i % 8 == 0):
+            list_ex.append(example[temp:i])
+            temp = i
+    words = []
+    for i in list_ex:
+        words.append(dic_table[i])
+    text = ''.join(words)
+    print(text)
 
-list_ex = []
-temp = 0
-for i in range(len(example)):
-    if (i != 0) and (i % 8 == 0):
-        list_ex.append(example[temp:i])
-        temp = i
-
-print(dic_table[list_ex[0]])
-
-
-
+Cod(input('Введите текст:\n'))
