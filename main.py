bd8d7c160534a7ccabbd0ee7386dab6a359b2331
Mikhail (laptop) <89531276499nokia@gmail.com>
2022-04-25 08:50:32 +0300

    послед сохр

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from config import dic_table
 
+example = r"\320\237\320\276\320\263\320\276\320\264\320\260.py".split('.')[0]
+
+list_ex = []
+temp = []
+# for i in range(4):
+#     temp.append(example[i])
+#     if i % 4 == 0:
+#         list_ex.append(''.join(temp))
+#         temp = []
+for i in range(8):
+    temp.append(example[i])
+    t = ''.join(temp)
+list_ex.append(t)
+
+print(t)
+print(list_ex)
+
+
